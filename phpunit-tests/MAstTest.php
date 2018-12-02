c81dced9c5a2b26616ddaacff0eb261c3ac7492d
kostochkin <kostochkin@rplab.ru>
2018-12-02 14:27:29 +0300

    some tests

--- phpunit-tests/MAstTest.php
+++ phpunit-tests/MAstTest.php
@@ -28,6 +28,41 @@ final class ASTtest extends TestCase {
 			$mvar->to_string());
   }
 	
+	public function test_MNum() : void  {
+		$mvar = new MNum(1);
+		$this->assertEquals(
+			"1",
+			$mvar->to_string());
+  }
+	
+	public function test_MNull() : void  {
+		$mvar = new MNull();
+		$this->assertEquals(
+			"null",
+			$mvar->to_string());
+  }
+
+	public function test_MParent() : void  {
+		$mvar = new MParent();
+		$this->assertEquals(
+			"parent",
+			$mvar->to_string());
+  }
+
+	public function test_MSelf() : void  {
+		$mvar = new MSelf();
+		$this->assertEquals(
+			"self",
+			$mvar->to_string());
+  }
+
+	public function test_MThis() : void  {
+		$mvar = new MThis();
+		$this->assertEquals(
+			"\$this",
+			$mvar->to_string());
+  }
+
 	public function test_MAssign() : void {
 		$massign = new MAssign(new MVar(new MId("foo")), new MString("bar"));
 		eval($massign->to_string() . ";");
